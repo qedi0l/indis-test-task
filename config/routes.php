5a8dce20657c596795f803d0e3d11bd39fa3206a
qedi0l <qediworks@gmail.com>
2024-07-12 09:02:30 +0300

    removed comments

--- config/routes.php
+++ config/routes.php
@@ -9,24 +9,6 @@ use App\Entity\Product;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
-    /*$routes->add('user/add', '/user/add')
-        ->controller([UserController::class, 'add']);
-
-    $routes->add('delete.user', '/user/delete/{id}')
-        ->controller([UserController::class, 'delete']);
-
-    $routes->add('user/add', '/user/add')
-        ->controller([UserController::class, 'add']);
-
-    $routes->add('user/addproduct', '/user/addproduct/{id}/{id_prod}')
-        ->controller([UserController::class, 'addProd']);
-
-    $routes->add('delete.user', '/user/delete/{id}')
-        ->controller([UserController::class, 'delete']);
-
-    $routes->add('product/add', '/product/add')
-        ->controller([ProductController::class, 'add']);
-        */
 
     $routes->add('book.create', '/book/create/{book_json}')
         ->controller([BookController::class, 'create']);
